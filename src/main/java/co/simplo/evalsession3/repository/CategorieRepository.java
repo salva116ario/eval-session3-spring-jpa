f2ce203eddfa0df8ef6bf86fb0647711102a8bff
SamuelJOBLON <samuel.joblon@gmail.com>
2018-12-14 11:17:14 +0100

    bizr2

--- src/main/java/co/simplo/evalsession3/repository/CategorieRepository.java
+++ src/main/java/co/simplo/evalsession3/repository/CategorieRepository.java
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 package co.simplo.evalsession3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import co.simplo.evalsession3.model.Categorie;
-import org.springframework.data.repository.CrudRepository;
-
-import org.springframework.stereotype.Repository;
-public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
-
-}
-=======
-package co.simplo.evalsession3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import co.simplo.evalsession3.model.Categorie;
 
 /**
@@ -24,4 +12,3 @@ import co.simplo.evalsession3.model.Categorie;
 public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
 
 }
->>>>>>> branch 'master' of https://github.com/salva116ario/eval-session3-spring-jpa.git
